ad1489bcb9e89f99957e85115b0ab4e31c7bfa59
rkaioo41-blip <rkaioo41@gmail.com>
2026-04-26 18:46:49 -0300

    Fix CORS production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173'  # Seu frontend Vue
-    
-    resource '*',
+    origins "https://jorgeluis.adv.br", "https://www.jorgeluis.adv.br"
+
+    resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      credentials: false
   end
 end
